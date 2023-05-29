749280fc7cd90eba0044a5aa2ad025285eb82772
kiana <kiana.sherafati@gmail.com>
2023-05-29 23:56:13 +0330

    feat : Added Analyze class

---
+++ src/Analyze.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Analyze {
+    private static int numberOfWords = 0;
+    private static HashMap<String, Integer> allWords = new HashMap<>();
+    private static int averageLengthOfAllWords = 0;
+    private static ArrayList<String> shortWord = new ArrayList<>();
+    private static ArrayList<String> longWord = new ArrayList<>();
+    private static int index;
+
+}
